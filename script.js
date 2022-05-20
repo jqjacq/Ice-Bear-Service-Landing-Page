6377a9a864c8aa6ecc07bd2d45bc54efa989a7ac
jqjacq <85534903+jqjacq@users.noreply.github.com>
2022-05-20 00:15:09 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+let icebeardescribe = document.getElementById("icebeardescribe")
+icebeardescribe.innerHTML = "Hello! I am Ice Bear. <br> Ice Bear was born in the North Pole and raised by a man named Yuri.<br>Ice Bear has two bros named Panda and Grizzly.They're my best friends.Amongst them, I am the youngest.<br>Ice Bear does all the chores for Panda and Grizzly. <br>Ice Bear loves to cook, speak multi-language, dance, engineer, and do martial arts.<br>"
+
+let headline = document.getElementById("headline")
+headline.innerHTML = "One of the best house cleaning services from the best bear."
+
+let daily = document.getElementById("daily")
+daily.innerHTML = "<span class='cost'>$20/day</span><br><br>Daily clean up of whole place. <br><br>Every 10th of the session, <br> you get 5 free session."
+
+let month = document.getElementById("monthly")
+monthly.innerHTML = "<span class='cost'>$50/month</span><br><br>Every first of the month.<br><br>Every year of the session,<br> you get 3 free session."
+
+let deepclean = document.getElementById("deepclean")
+deepclean.innerHTML = "<span class='cost'>$100/each time</span><br><br>Deep cleaning.<br><br>Every 5th of deep cleaning,<br> you get 1 free."
+
 function changeNav() {
     let navlink = document.getElementById("navlink")
     if (navlink.style.display === "block") {
